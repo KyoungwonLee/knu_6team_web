07d5a43731105004cc456dc82d03743721ce67d1
0l21owon <lkw1142@naver.com>
2024-08-14 15:31:06 +0900

    오류수정

--- public/mypage/index.js
+++ public/mypage/index.js
@@ -8,3 +8,25 @@ function handleTokenVerification(response) {
     window.location.href = "http://localhost:7999/signin";
   }
 }
+
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const userToken = await localStorage.getItem("token");
+    console.log("token", userToken);
+
+    const sendResult = await fetch("/api/user/token", {
+      method: "post",
+      body: JSON.stringify({ userToken }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (sendResult.ok) {
+      const result = await sendResult.json();
+      // result = {isVerify: true }()
+      handleTokenVerification(result);
+    } else {
+      console.log("유효성 검사 실패");
+    }
+  } catch (err) {}
+});
