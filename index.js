777d611fe90f4a63a22a5a0b1e78cc0e309794b7
daaoooy <y020d226y@daum.net>
2024-08-14 14:44:21 +0900

    token 유효성 검사

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-// (+)
-console.log("exec express");
+// // (+)
+// console.log("exec express");
 
 app.use("/api", apiController);
 
