74aec7f22f4dd2f84d4bb07c0289c46085641837
Jisoo <jskim3936@gmail.com>
2024-08-15 10:55:39 +0900

    장바구니 이전까지 완료

--- public/product/detail/index.js
+++ public/product/detail/index.js
@@ -2,6 +2,13 @@ const url = window.location.href;
 const params = new URLSearchParams(new URL(url).search);
 const id = params.get("id");
 
+// document.getElementById("buy").onclick = function (){
+//   window.location.href = "/cart";
+// }
+// document.getElementById("cart").onclick = function (){
+//   alert("장바구니에 담겼습니다.");
+// }
+
 const fetchProduct = async (id) => {
   try {
     const fetchResult = await fetch(`/api/product/detail/${id}`);
@@ -35,7 +42,39 @@ renderProduct = async () => {
             <div>[가격]: ${v.price}</div>
             <div>[상세설명]: ${v.description}</div>
             <div>[재고]: ${v.stock}</div>
+                
             `;
+    // <button id="cart" onclick="alert('장바구니에 담겼습니다.')">장바구니</button>
+    const cartButtonElem = document.createElement("button");
+    cartButtonElem.innerHTML = "장바구니";
+    cartButtonElem.onclick = () => {
+      alert("장바구니에 담겼습니다.");
+    };
+    const buyButtonElem = document.createElement("button");
+    buyButtonElem.innerHTML = "구매하기";
+    buyButtonElem.onclick = () => {
+      window.location.href = "/cart";
+    };
+    const labelTag = document.createElement("label");
+    labelTag.innerHTML = "구매수량";
+    const inputTag = document.createElement("input");
+    inputTag.setAttribute("type", "number");
+    inputTag.addEventListener("input", (e) => {
+      const value = parseInt(inputTag.value, 10);
+
+      // 값이 숫자인지 확인하고 유효한 범위로 제한합니다.
+      if (isNaN(value) || value < 0) {
+        inputTag.value = 0;
+      } else if (value > v.stock) {
+        inputTag.value = v.stock;
+      }
+    });
+    const brTag = document.createElement("br");
+    itemElem.append(labelTag);
+    itemElem.append(inputTag);
+    itemElem.append(brTag);
+    itemElem.append(cartButtonElem);
+    itemElem.append(buyButtonElem);
     productWrapper.append(itemElem);
   }
 };
